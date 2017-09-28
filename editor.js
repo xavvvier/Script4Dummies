97919db84aa7ca2ac1025ccf02e9cc14f0a72bcb
falarcond <edwin.alarcon1986@hotmail.com>
2017-09-28 18:18:35 -0500

    Parameter configuration

--- editor.js
+++ editor.js
@@ -1,45 +1,61 @@
 
 angular.module('scriptApp', [])
-.controller('scriptCtrl', function(){
-    var model = this;
-    model.Name = '';
-    model.Description = '';
+    .controller('scriptCtrl', function ($scope) {
 
-    model.parameters = [
-        {id: 'initDate', name:'Initial Date'},
-        {id: 'endDate', name:'End Date'}
-    ];
+        $scope.Name = '';
+        $scope.Description = '';
 
-    model.addParameter = function(){
-        model.parameters.push({});
-    };
+        $scope.parameterTypes = [
+            'acl',
+            'constant',
+            'sql',
+            'fields',
+            'search'
+        ];
+        $scope.dataTypes = ['Date', 'DateTime', 'Text', 'User', 'Number'];
+        $scope.typeartifactid = '';
 
-    init(); 
-});
+        $scope.parameters = [
+            // { parameterType: 'constant', id: 'initDate', name: 'Initial Date', dataType: 'Date' },
+            // { parameterType: 'constant', id: 'endDate', name: 'End Date' },
+            // { parameterType: 'constant', id: 'price', name: 'Product price', type: 'Number', precision: 2, scale: 10 },
+            // { parameterType: 'sql', id: 'endDate', name: 'End Date', sql: 'select top 10 ArtifactID from Artifact' }
+        ];
 
-function init(){
+        $scope.addParameter = function () {
+            $scope.parameters.push({});
+        };
+
+        $scope.clearParameters = function () {
+            $scope.parameters = [];
+        }
+
+        init();
+    });
+
+function init() {
     var editor = ace.edit("sqlEditor");
     editor.setTheme("ace/theme/solarized_dark");
     editor.getSession().setMode("ace/mode/sqlserver");
 
 
     var sqlScript = "CREATE PROCEDURE dbo.Test2 \n " +
-" \n " +
-"    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"    INSERT INTO #t VALUES (2 \n " +
-"    SELECT Test2Col = x FROM # \n " +
         " \n " +
-"CREATE PROCEDURE dbo.Tes \n " +
-" \n " +
-"    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"    INSERT INTO #t VALUES (1 \n " +
-"    SELECT Test1Col = x FROM # \n " +
-"EXEC Test2 \n " +
-" \n " +
-"CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"INSERT INTO #t VALUES (99) \n " +
-"EXEC Test1 \n " +
-"GO";
+        "    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
+        "    INSERT INTO #t VALUES (2 \n " +
+        "    SELECT Test2Col = x FROM # \n " +
+        " \n " +
+        "CREATE PROCEDURE dbo.Tes \n " +
+        " \n " +
+        "    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
+        "    INSERT INTO #t VALUES (1 \n " +
+        "    SELECT Test1Col = x FROM # \n " +
+        "EXEC Test2 \n " +
+        " \n " +
+        "CREATE TABLE #t(x INT PRIMARY KEY) \n " +
+        "INSERT INTO #t VALUES (99) \n " +
+        "EXEC Test1 \n " +
+        "GO";
 
     editor.setValue(sqlScript);
     editor.gotoLine(1);
