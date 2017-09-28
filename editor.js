30933f7d5aad71d9df47c2d59276243c41e64114
xavvvier <alternar64@gmail.com>
2017-09-28 17:23:14 -0500

    Read values from text area

--- editor.js
+++ editor.js
@@ -22,25 +22,12 @@ function init(){
     editor.setTheme("ace/theme/solarized_dark");
     editor.getSession().setMode("ace/mode/sqlserver");
 
-
-    var sqlScript = "CREATE PROCEDURE dbo.Test2 \n " +
-" \n " +
-"    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"    INSERT INTO #t VALUES (2 \n " +
-"    SELECT Test2Col = x FROM # \n " +
-        " \n " +
-"CREATE PROCEDURE dbo.Tes \n " +
-" \n " +
-"    CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"    INSERT INTO #t VALUES (1 \n " +
-"    SELECT Test1Col = x FROM # \n " +
-"EXEC Test2 \n " +
-" \n " +
-"CREATE TABLE #t(x INT PRIMARY KEY) \n " +
-"INSERT INTO #t VALUES (99) \n " +
-"EXEC Test1 \n " +
-"GO";
-
+    var targetElement = document.getElementById('targetElement');
+    var script = targetElement.value;
+    var parser = new DOMParser();
+    var xmlScript = parser.parseFromString(script, "text/xml");
+    var actionNode = xmlScript.firstChild.getElementsByTagName('action')[0];
+    var sqlScript = actionNode.firstChild.wholeText;
     editor.setValue(sqlScript);
     editor.gotoLine(1);
 
