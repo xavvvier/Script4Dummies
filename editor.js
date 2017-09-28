ddd3e8898fdc3c5313c5efed950e3f455d44619a
xavvvier <alternar64@gmail.com>
2017-09-28 18:08:51 -0500

    Started read operation

--- editor.js
+++ editor.js
@@ -1,34 +1,61 @@
 
 angular.module('scriptApp', [])
-.controller('scriptCtrl', function(){
-    var model = this;
-    model.Name = '';
-    model.Description = '';
+.controller('scriptCtrl', function($scope){
 
-    model.parameters = [
-        {id: 'initDate', name:'Initial Date'},
-        {id: 'endDate', name:'End Date'}
-    ];
+    var editor = ace.edit("sqlEditor");
 
-    model.addParameter = function(){
+    $scope.script = {
+        Name: '',
+        Description: '',
+        parameters: [
+            {id: 'initDate', name:'Initial Date'},
+            {id: 'endDate', name:'End Date'}
+        ]
+    };
+
+    $scope.addParameter = function(){
         model.parameters.push({});
     };
 
-    init(); 
+    $scope.read = function () {
+        var targetElement = document.getElementById('targetElement');
+        loadScript(targetElement, $scope.script);
+    };
+
+    $scope.init = function() {
+        editor.setTheme("ace/theme/solarized_dark");
+        editor.getSession().setMode("ace/mode/sqlserver");
+        //$scope.read();
+    }
+
+    $scope.init(); 
+
+    function loadScript(element, script){
+        var scriptDefinition = targetElement.value;
+        var parser = new DOMParser();
+        var xmlScript = parser.parseFromString(scriptDefinition, "text/xml");
+        var scriptNode = xmlScript.firstChild;
+
+        //Script Basic Properties
+        script.Name = nodeText(scriptNode.getElementsByTagName('name')[0]);
+        script.Description = nodeText(scriptNode.getElementsByTagName('description')[0]);
+        script.Category = nodeText(scriptNode.getElementsByTagName('category')[0]);
+
+        //Script parameters
+        var inputNode = xmlScript.getElementsByTagName('input')[0];
+        var parameterNodes = inputNode.children;
+        for (var i = 0, len = parameterNodes.length; i < len; i++) {
+            var parameterNode = parameterNodes[i];
+            console.log(parameterNode.nodeName);
+        }
+        var actionNode = scriptNode.getElementsByTagName('action')[0];
+        var sqlScript = actionNode.firstChild.wholeText;
+        editor.setValue(sqlScript);
+        editor.gotoLine(1);
+    }
+
+    function nodeText(node){
+        return node.firstChild.nodeValue;
+    }
 });
 
-function init(){
-    var editor = ace.edit("sqlEditor");
-    editor.setTheme("ace/theme/solarized_dark");
-    editor.getSession().setMode("ace/mode/sqlserver");
-
-    var targetElement = document.getElementById('targetElement');
-    var script = targetElement.value;
-    var parser = new DOMParser();
-    var xmlScript = parser.parseFromString(script, "text/xml");
-    var actionNode = xmlScript.firstChild.getElementsByTagName('action')[0];
-    var sqlScript = actionNode.firstChild.wholeText;
-    editor.setValue(sqlScript);
-    editor.gotoLine(1);
-
-}
